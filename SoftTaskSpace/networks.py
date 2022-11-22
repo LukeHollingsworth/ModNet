55e0669aa313d552ab62fbb0dad6929129279aa0
LukeHollingsworth <lukehollingsworth99@hotmail.com>
2022-11-22 16:21:36 +0000

    soft task space experiment added

---
+++ SoftTaskSpace/networks.py
@@ -0,0 +1,199 @@
+import numpy as np
+import matplotlib 
+from matplotlib import rcParams
+import matplotlib.pyplot as plt
+from cycler import cycler 
+plt.style.use("seaborn")
+rcParams['figure.dpi']= 300
+rcParams['axes.labelsize']=5
+rcParams['axes.labelpad']=2
+rcParams['axes.titlepad']=3
+rcParams['axes.titlesize']=5
+rcParams['axes.xmargin']=0
+rcParams['axes.ymargin']=0
+rcParams['xtick.labelsize']=4
+rcParams['ytick.labelsize']=4
+rcParams['grid.linewidth']=0.5
+rcParams['legend.fontsize']=4
+rcParams['lines.linewidth']=1
+rcParams['xtick.major.pad']=2
+rcParams['xtick.minor.pad']=2
+rcParams['ytick.major.pad']=2
+rcParams['ytick.minor.pad']=2
+rcParams['xtick.color']='grey'
+rcParams['ytick.color']='grey'
+rcParams['figure.titlesize']='medium'
+rcParams['axes.prop_cycle']=cycler('color', ['#66c2a5','#fc8d62','#8da0cb','#e78ac3','#a6d854','#ffd92f','#e5c494','#b3b3b3'])
+import random
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.nn import functional as F
+from torch import optim
+from tqdm.autonotebook import tqdm
+import time
+
+class simple_network(nn.Module):
+    def __init__(self, hyperparameters=None):
+        if hyperparameters == None:
+            hyperparameters = self.get_default_hyperparameters()
+        self.hidden_size = hyperparameters['hidden_size']
+        super(simple_network, self).__init__()
+
+        self.fc1 = nn.Linear(4, self.hidden_size)
+        self.fc2 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc3 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc4 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc5 = nn.Linear(self.hidden_size, 1)
+
+        # Initialise hyperparameter
+        self.N_train = hyperparameters['N_train']
+        self.N_test = hyperparameters['N_test']
+        self.epochs = hyperparameters['epochs']
+        self.lr = hyperparameters['lr']
+        self.batch_size = hyperparameters['batch_size']
+        self.train_mode = hyperparameters['train_mode']
+        self.fraction = hyperparameters['fraction']
+
+        # Initialises some attributes
+        self.optimizer = optim.Adam(self.parameters(), lr=self.lr)
+        self.type_of_network = 'simple_network'
+
+        # Arrays for later analysis
+        self.hist = []
+        self.RI = [[],[],[],[],[]]
+        self.Itask1 = [[],[],[],[],[]]
+        self.Itask2 = [[],[],[],[],[]]
+
+        # Training and testing data 
+        self.set_data()
+        self.initialise_biases()
+        self.hist.append(self.abs_error())
+
+        self.task1_description = '$y = {}x + {}$'.format(self.A1, self.C1_test[0])
+        self.task2_description = '$y = {}x + {}$'.format(self.A2, self.C2_test[0])
+
+    def get_default_hyperparameters(self):
+        hps = {'N_train' : 1000, #size of training dataset 
+               'N_test' : 100, #size of test set x
+               'lr' : 0.001, #SGD learning rate 
+               'epochs' : 10, #training epochs
+               'batch_size' : 10,  #batch size (large will probably fail)           
+               'context_location' : 'start',  #where the feed in the task context 'start' vs 'end'
+               'train_mode' : 'random', #training mode 'random' vs 'replay' 
+               'second_task' : 'prod', #first task adds x+y, second task 'prod' = xy or 'add1.5' = x+1.5y
+               'fraction' : 0.50, #fraction of training data for tasks 1 vs task 2
+               'hidden_size' : 100} #hidden layer width 
+        return hps
+    
+    def set_data(self):
+        self.N_task1 = int(self.fraction*self.N_train)
+        self.N_task2 = self.N_train - self.N_task1
+
+        # For the line Ax + By + C = 0
+        self.A1, self.A2 = 0.5, 5
+        self.B1, self.B2 = -1, -1
+        self.C1_train, self.C2_train = np.zeros(self.N_task1), np.zeros(self.N_task2)
+        self.C1_test, self.C2_test = np.zeros(self.N_test), np.zeros(self.N_test)
+
+        self.x1_train = np.concatenate((np.random.uniform(0,1,(self.N_task1, 2)), np.ones((self.N_task1, 1)), np.zeros((self.N_task1,1))), axis=1)
+        self.x2_train = np.concatenate((np.random.uniform(0,1,(self.N_task2, 2)), np.zeros((self.N_task2, 1)), np.ones((self.N_task2,1))), axis=1)
+        self.x1_test = np.concatenate((np.random.uniform(0,1,(self.N_test, 2)), np.ones((self.N_test, 1)), np.zeros((self.N_test,1))), axis=1)
+        self.x2_test = np.concatenate((np.random.uniform(0,1,(self.N_test, 2)), np.zeros((self.N_test, 1)), np.ones((self.N_test,1))), axis=1)
+
+        self.y1_train = np.abs(self.A1*self.x1_train[:,0] + self.B1*self.x1_train[:,1] + self.C1_train) / np.sqrt(self.A1**2 + self.B1**2)
+        self.y2_train = np.abs(self.A2*self.x2_train[:,0] + self.B2*self.x2_train[:,1] + self.C2_train) / np.sqrt(self.A2**2 + self.B2**2)
+        self.y1_test = np.abs(self.A1*self.x1_test[:,0] + self.B1*self.x1_test[:,1] + self.C1_test) / np.sqrt(self.A1**2 + self.B1**2)
+        self.y2_test = np.abs(self.A2*self.x2_test[:,0] + self.B2*self.x2_test[:,1] + self.C2_test) / np.sqrt(self.A2**2 + self.B2**2)
+
+        self.x_train = np.concatenate((self.x1_train, self.x2_train))
+        self.y_train = np.concatenate((self.y1_train, self.y2_train))
+
+        self.x_train = torch.from_numpy(self.x_train).float()
+        self.x1_train = torch.from_numpy(self.x1_train).float()
+        self.x2_train = torch.from_numpy(self.x2_train).float()
+        self.x1_test = torch.from_numpy(self.x1_test).float()
+        self.x2_test = torch.from_numpy(self.x2_test).float()
+        
+        self.y_train = torch.from_numpy(self.y_train).float().unsqueeze(1)
+        self.y1_train = torch.from_numpy(self.y1_train).float().unsqueeze(1)
+        self.y2_train = torch.from_numpy(self.y2_train).float().unsqueeze(1)
+        self.y1_test = torch.from_numpy(self.y1_test).float().unsqueeze(1)
+        self.y2_test = torch.from_numpy(self.y2_test).float().unsqueeze(1)
+
+    def rule(self, m, c, N):
+        x1 = 0
+        x2 = 1
+        r1 = [x1, m*x1+c]
+        r2 = [x2, m*x2+c]
+
+        return np.tile(r1, (N, 1)), np.tile(r2, (N, 1))
+
+    def initialise_biases(self): #probably unneccesary, set biases initially to zero
+        torch.nn.init.zeros_(self.fc1.bias)
+        torch.nn.init.zeros_(self.fc2.bias)
+        torch.nn.init.zeros_(self.fc3.bias)
+        torch.nn.init.zeros_(self.fc4.bias)
+        torch.nn.init.zeros_(self.fc5.bias)
+
+    def forward(self, input, mode='normal'):
+        x = nn.functional.relu(self.fc1(input))        
+        x1 = nn.functional.relu(self.fc2(x))        
+        x2 = nn.functional.relu(self.fc3(x1))
+        x3 = nn.functional.relu(self.fc4(x2))
+        x4 = nn.functional.relu(self.fc5(x3))
+        if mode == 'normal':
+            return x4
+        elif mode == 'other':
+            return x, x1, x2, x3, x4
+
+    def abs_error(self):
+        task1_error = (self.forward(self.x1_test) - self.y1_test).abs().mean(dim=0).item()
+        task2_error = (self.forward(self.x2_test) - self.y2_test).abs().mean(dim=0).item()
+        return [task1_error, task2_error]
+
+    def train_model(self):
+        for epoch in range(self.epochs):
+            for i in range(int(self.N_train/self.batch_size)): #input.shape == [2]
+                idx = np.random.choice(self.N_train,self.batch_size,replace=False)
+                self.do_train_step(idx)
+            self.eval() #test/evaluation model 
+            with torch.no_grad():
+                self.hist.append(self.abs_error())     
+
+    def do_train_step(self, idx):
+        sample=self.x_train[idx] 
+        self.train() # we move the model to train regime because some models have different train/test behavior, e.g., dropout.
+        self.optimizer.zero_grad()
+        output = self.forward(sample)
+        loss = F.mse_loss(output,self.y_train[idx])
+        loss.backward()
+        self.optimizer.step()
+
+    def get_RI(self):
+        self.RI = [[],[],[],[],[]]
+        self.Itask1 = [[],[],[],[],[]]
+        self.Itask2 = [[],[],[],[],[]]
+
+        hidden_task1 = self.forward(self.x1_test, mode='other')        
+        hidden_task2 = self.forward(self.x2_test, mode='other') 
+        
+        error_task1 = F.mse_loss(self.y1_test, hidden_task1[-1])
+        error_task2 = F.mse_loss(self.y2_test, hidden_task2[-1])
+        
+        for i in range(len(hidden_task1)):
+            hidden_task1[i].retain_grad()
+            hidden_task2[i].retain_grad()
+            
+        error_task1.backward()
+        error_task2.backward()    
+
+        for i in range(len(hidden_task1)):
+            Itask1 = (((hidden_task1[i] * hidden_task1[i].grad)**2).mean(0)).detach().numpy()
+            Itask2 = (((hidden_task2[i] * hidden_task2[i].grad)**2).mean(0)).detach().numpy()
+            Itask1[Itask1 < np.mean(Itask1)/10]=0
+            Itask2[Itask2 < np.mean(Itask2)/10]=0
+            RI_ = (Itask1 - Itask2) / (Itask1 + Itask2)
+            self.Itask1[i].extend(list(Itask1)) 
+            self.Itask2[i].extend(list(Itask2))
+            self.RI[i].extend(list(RI_))
---
+++ SoftTaskSpace/utils.py
@@ -0,0 +1,92 @@
+from copy import deepcopy
+import random
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.autograd import Variable
+from torchvision import datasets
+from torch.utils.data import Dataset
+from tqdm.autonotebook import tqdm
+import time 
+import numpy as np
+import matplotlib 
+import matplotlib.pyplot as plt
+
+def train_multiple(model_class, hyperparameters = None,  N_models=20, ):
+    models = []    
+    
+    if model_class == 'simple_network':
+        from networks import simple_network
+        for _ in tqdm(range(N_models), desc="Model"):
+            fail_count = 0
+            current_model_successful = False
+            while current_model_successful == False:
+                if fail_count >= 10:
+                    print("\n This model doesn't train well, aborting")
+                    return models
+                model = simple_network(hyperparameters)
+                model.train_model()
+                if model.abs_error()[0]<0.05 and model.abs_error()[1]<0.05:
+                    model.get_RI()
+                    models.append(model)
+                    current_model_successful = True
+                else:
+                    fail_count += 1
+        return models
+
+def plot_training(models, title=None, axis_scale='linear'):  #only works for simple_network lists, not MNIST_networks
+    if models[0].type_of_network == 'simple_network':    
+        fig, ax = plt.subplots(figsize = (2,1.5))
+        task1 = np.zeros(len(np.array(models[0].hist)))
+        task2 = np.zeros(len(np.array(models[0].hist)))    
+        for i in range(len(models)):
+            ax.plot(np.array(models[i].hist)[:,0], linewidth=0.5, alpha=0.2, c='C0')
+            task1 += np.array(models[i].hist)[:,0]
+            ax.plot(np.array(models[i].hist)[:,1], linewidth=0.5, alpha=0.2, c='C1')
+            task2 += np.array(models[i].hist)[:,1]
+        task1, task2 = task1/len(models), task2/len(models)
+        ax.plot(task1, linewidth=1, c='C0', label = r'Task 1: %s' %models[-1].task1_description )
+        ax.plot(task2, linewidth=1, c='C1', label =r'Task 2: %s' %models[-1].task2_description)
+        ax.set_ylabel('Absolute test error')
+        ax.set_xlabel('Epochs')
+        if models[-1].train_mode == 'replay':
+            ax.axvspan(0, models[-1].epochs,color='C0', alpha=0.1)  #vertical shading
+            ax.axvspan(models[-1].epochs, 2*models[-1].epochs,color='C1', alpha=0.1)  #vertical shading
+        ax.legend()
+        if title != None:
+            fig.suptitle("%s" %title)
+        if axis_scale == 'log':
+            ax.set_xscale('log')
+        plt.show()
+        return
+
+def plot_RI(models, show_threshold=False, title=None):  #only works for simple_network lists, not MNIST_networks
+    
+    if models[0].type_of_network == 'simple_network':
+        RI = [[],[],[],[],[]]
+        for model in models:
+            for i in range(len(RI)):
+                RI[i].extend(list(model.RI[i]))
+        fig, axs = plt.subplots(1,4,sharey = True, figsize = (4,0.8))
+        for i in range(4):
+            n, bins, patches = axs[i].hist(RI[i], weights=np.ones(len(RI[i])) / len(RI[i]),bins=np.linspace(-1,1,11))
+            bin_centre = [(bin_right + bin_left)/2 for (bin_right, bin_left) in zip(list(bins[1:]),list(bins[:-1]))]
+            col = (bin_centre - min(bin_centre))/(max(bin_centre) - min(bin_centre))
+            cm = matplotlib.colors.LinearSegmentedColormap.from_list('my_cmap',['C1','C0'], N=1000)
+            for c, p in zip(col, patches):
+                    plt.setp(p, 'facecolor', cm(c))
+            plt.gca().yaxis.set_major_formatter(matplotlib.ticker.PercentFormatter(1))
+            axs[i].set_title("Hidden layer %g" %(i+1))
+            axs[i].set_xlim([-1,1])
+            axs[i].set_xlabel(r'$\mathcal{RI}$')
+            if i == 0:
+                axs[i].set_ylabel('Proportion')            
+            if i == 3 and show_threshold == True: 
+                axs[i].axvline(0.9,color='r',linestyle='--',linewidth=0.8)
+                axs[i].axvline(-0.9,color='r',linestyle='--',linewidth=0.8)
+        for i in range(4):
+            axs[i].text(0.51,axs[i].get_ylim()[-1]*0.92, r"+ %g%%" %int((100*(np.sum(np.isnan(np.array(RI[i])))/len(RI[i])))), fontdict = {'color':'grey', 'fontsize':4})
+        if title != None:
+            fig.suptitle("%s" %title)
+        plt.show()
+        return
